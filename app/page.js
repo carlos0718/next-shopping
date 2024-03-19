53af066702485fd3bc6ec1ad6218573589b7d745
c.sipiran07@gmail.com <carlos0718>
2024-03-19 20:35:39 -0300

    change navbar mui

--- app/page.js
+++ app/page.js
@@ -1,27 +1,16 @@
-import Box from "@mui/joy/Box";
-import Header from "./ui/header/header";
-import Cards from "./ui/renderCards/cards";
 import "./globals.css";
+import ResponsiveAppBar from "./ui/header/header";
+import Cards from "./ui/renderCards/cards";
 
 export default function Home() {
 	return (
 		<>
-			<Header />
+			<ResponsiveAppBar />
 			<main className='container m-auto p-10'>
 				<div className='mb-5'>
 					<input type='text' placeholder='Search' className='border-2 border-gray-300 p-2 rounded-md' />
 				</div>
-				<Box
-					sx={{
-						width: "100%",
-						maxWidth: 500,
-						display: "grid",
-						gridTemplateColumns: "repeat(auto-fill, minmax(300px, 4fr))",
-						gap: 2,
-					}}
-				>
-					<Cards />
-				</Box>
+				<Cards />
 			</main>
 		</>
 	);
